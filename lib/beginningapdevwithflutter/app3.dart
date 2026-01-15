6c2c6f3a8fd40d0e194422e63974722d4f27d0a0
Ovy_Vintila <ovyvin@gmail.com>
2026-01-15 12:45:16 +0000

    2026-01-15_flutter_2

--- lib/beginningapdevwithflutter/app3.dart
+++ lib/beginningapdevwithflutter/app3.dart
@@ -4,9 +4,41 @@ class MyApp3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
-      body: Row(
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment
+            .stretch, //when use this , the width of the container is not respected
         children: [
-          Text('app'),
+          Container(
+            color: Colors.yellow,
+            height: 100.0,
+            width: 100.0,
+            //margin: EdgeInsets.all(20),
+            //margin: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+            //margin: EdgeInsets.fromLTRB(50, 20, 10, 50),
+            child: Text('FlutterApp1'),
+          ),
+          Container(
+            color: Colors.blue,
+            height: 100.0,
+            //width: 300.0,
+            width: double.infinity,
+            //width: 100.0,
+            //margin: EdgeInsets.all(20),
+            //margin: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+            //margin: EdgeInsets.fromLTRB(50, 20, 10, 50),
+            child: Text('FlutterApp2'),
+          ),
+          Container(
+            color: Colors.green,
+            height: 100.0,
+            width: 100.0,
+            //margin: EdgeInsets.all(20),
+            //margin: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+            //margin: EdgeInsets.fromLTRB(50, 20, 10, 50),
+            child: Text('FlutterApp2'),
+          ),
         ],
       ),
     );
