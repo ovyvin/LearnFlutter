db87b181ecb8d23e8b1e7a717de047cc22f52990
Ovy_Vintila <ovyvin@gmail.com>
2026-01-23 14:46:00 +0000

    20260123_flutter_4

--- lib/beginningapdevwithflutter/DiceApp/app1.dart
+++ lib/beginningapdevwithflutter/DiceApp/app1.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+class DiceApp1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset(
+            '/assets/images/dice1.png',
+          ),
+        ),
+        Expanded(
+          child: Image.asset('/assets/images/dice2.png'),
+        ),
+      ],
+    );
+  }
+}
