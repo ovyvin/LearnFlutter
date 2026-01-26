82bd354ad3d9a70f9414b3fff0d28c40dd8ac042
Ovy_Vintila <ovyvin@gmail.com>
2026-01-26 11:45:26 +0000

    20260126_flutter_2

--- lib/beginningapdevwithflutter/DiceApp/app1.dart
+++ lib/beginningapdevwithflutter/DiceApp/app1.dart
@@ -1,24 +1,37 @@
+import 'dart:html';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class DiceApp1 extends StatelessWidget {
+class DiceApp1 extends StatefulWidget {
+  @override
+  _DiceApp1State createState() => _DiceApp1State();
+}
+
+class _DiceApp1State extends State<DiceApp1> {
   @override
   Widget build(BuildContext context) {
+    var leftDiceNumber = 2;
     return Center(
       child: Row(
         children: [
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Image.asset(
-                '/assets/images/dice1.png',
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset(
+                  '/assets/images/dice$leftDiceNumber.png',
+                ),
               ),
             ),
           ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Image.asset('/assets/images/dice2.png'),
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset('/assets/images/dice2.png'),
+              ),
             ),
           ),
         ],
