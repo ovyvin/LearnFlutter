106a715bb09d7eedb29e309aa91cf022428f3ef2
Ovy_Vintila <ovyvin@gmail.com>
2026-01-26 07:07:20 +0000

    20260126_flutter_1

--- lib/beginningapdevwithflutter/DiceApp/app1.dart
+++ lib/beginningapdevwithflutter/DiceApp/app1.dart
@@ -4,17 +4,25 @@ import 'package:flutter/material.dart';
 class DiceApp1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset(
-            '/assets/images/dice1.png',
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset(
+                '/assets/images/dice1.png',
+              ),
+            ),
           ),
-        ),
-        Expanded(
-          child: Image.asset('/assets/images/dice2.png'),
-        ),
-      ],
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('/assets/images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
