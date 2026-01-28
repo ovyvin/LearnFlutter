083ecd23b02c8bcd4431006efe2f52699206ad67
Ovy_Vintila <ovyvin@gmail.com>
2026-01-28 12:38:02 +0000

    20260128_flutter_3

--- lib/beginningapdevwithflutter/Xylophone/xylophoneapp.dart
+++ lib/beginningapdevwithflutter/Xylophone/xylophoneapp.dart
@@ -10,15 +10,110 @@ class XylophoneApp extends StatefulWidget {
 class _XylophoneAppState extends State<XylophoneApp> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {
-          final player = AssetsAudioPlayer();
-          player.open(
-            Audio("assets/audios/note1.wav"),
-          );
-        },
-        child: Text('ClickMe'),
+    return SafeArea(
+      child: Center(
+        child: Row(
+          children: [
+            TextButton(
+              onPressed: () {
+                final player = AssetsAudioPlayer();
+                player.open(
+                  Audio("assets/audios/note1.wav"),
+                );
+              },
+              child: Text(
+                'DO',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                final player = AssetsAudioPlayer();
+                player.open(
+                  Audio("assets/audios/note2.wav"),
+                );
+              },
+              child: Text(
+                'RE',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                final player = AssetsAudioPlayer();
+                player.open(
+                  Audio("assets/audios/note3.wav"),
+                );
+              },
+              child: Text(
+                'MI',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                final player = AssetsAudioPlayer();
+                player.open(
+                  Audio("assets/audios/note4.wav"),
+                );
+              },
+              child: Text(
+                'FA',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                final player = AssetsAudioPlayer();
+                player.open(
+                  Audio("assets/audios/note5.wav"),
+                );
+              },
+              child: Text(
+                'SOL',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                final player = AssetsAudioPlayer();
+                player.open(
+                  Audio("assets/audios/note6.wav"),
+                );
+              },
+              child: Text(
+                'LA',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                final player = AssetsAudioPlayer();
+                player.open(
+                  Audio("assets/audios/note7.wav"),
+                );
+              },
+              child: Text(
+                'SI',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
