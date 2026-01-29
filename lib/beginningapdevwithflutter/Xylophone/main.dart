41d3febe9786fcdc0a875cacecdea2ae2cae7bfa
Ovy_Vintila <ovyvin@gmail.com>
2026-01-29 08:34:38 +0000

    20260129_flutter_2

---
+++ lib/beginningapdevwithflutter/Xylophone/main.dart
@@ -0,0 +1,32 @@
+import 'package:learnflutter/beginningapdevwithflutter/Xylophone/xylophoneapp.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+
+void main() {
+  runApp(DiceApp());
+}
+
+class DiceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Xylophone',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: XylophoneApp(),
+      ),
+    );
+  }
+}
