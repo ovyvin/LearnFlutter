92d7daa67eece3b9a8b53a69d7827de704fc3783
Ovy_Vintila <ovyvin@gmail.com>
2026-01-30 20:28:26 +0000

    20250120_flutter_2

--- lib/beginningapdevwithflutter/Magic8Ball/main.dart
+++ lib/beginningapdevwithflutter/Magic8Ball/main.dart
@@ -1,5 +1,7 @@
-import 'package:learnflutter/beginningapdevwithflutter/Magic8Ball/magic8.dart';
+import 'package:learnflutter/beginningapdevwithflutter/Xylophone/xylophoneapp.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() {
   runApp(DiceApp());
@@ -11,19 +13,19 @@ class DiceApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.white,
         appBar: AppBar(
           centerTitle: true,
           title: Text(
-            'Magic8',
+            'Xylophone',
             textAlign: TextAlign.center,
             style: TextStyle(
               fontWeight: FontWeight.bold,
             ),
           ),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
         ),
-        body: Magic8App(),
+        body: XylophoneApp(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:learnflutter/beginningapdevwithflutter/Xylophone/xylophoneapp.dart';
+import 'package:learnflutter/beginningapdevwithflutter/Magic8Ball/magic8.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
@@ -17,7 +18,7 @@ class DiceApp extends StatelessWidget {
         appBar: AppBar(
           centerTitle: true,
           title: Text(
-            'Xylophone',
+            'Magic8Ball',
             textAlign: TextAlign.center,
             style: TextStyle(
               fontWeight: FontWeight.bold,
@@ -25,7 +26,7 @@ class DiceApp extends StatelessWidget {
           ),
           backgroundColor: Colors.blue,
         ),
-        body: XylophoneApp(),
+        body: Magic8App(),
       ),
     );
   }
