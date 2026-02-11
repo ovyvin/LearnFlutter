81719b6b993b4d7a9aa8cb8927ea03267a05bbef
Ovy_Vintila <ovyvin@gmail.com>
2026-02-11 07:11:29 +0000

    20260211_flutter_2

--- lib/beginningapdevwithflutter/PokeApp/pokequiz.dart
+++ lib/beginningapdevwithflutter/PokeApp/pokequiz.dart
@@ -10,7 +10,28 @@ class _PokeQuizState extends State<PokeQuiz> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('aa'),
+        Expanded(
+          flex: 3,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            child: Image.asset('/assets/images/poke1.png'),
+          ),
+        ),
+        Expanded(
+            flex: 2,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+              child: Text(
+                'Here will be the question',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontFamily: 'Pacifico',
+                  fontSize: 20.0,
+                ),
+              ),
+            )),
+
+        //Expanded(),
       ],
     );
   }
