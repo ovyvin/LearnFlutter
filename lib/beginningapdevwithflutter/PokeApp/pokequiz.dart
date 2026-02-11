3500b4d79de0d4d8fcc157f15ce5d8533b95c814
Ovy_Vintila <ovyvin@gmail.com>
2026-02-11 08:37:59 +0000

    20260211_flutter_3

--- lib/beginningapdevwithflutter/PokeApp/pokequiz.dart
+++ lib/beginningapdevwithflutter/PokeApp/pokequiz.dart
@@ -9,29 +9,66 @@ class _PokeQuizState extends State<PokeQuiz> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Expanded(
-          flex: 3,
+          flex: 5,
           child: Padding(
             padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
             child: Image.asset('/assets/images/poke1.png'),
           ),
         ),
         Expanded(
-            flex: 2,
+            //flex: 2,
             child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+          padding: EdgeInsets.all(10.0),
+          child: Text(
+            'Here will be the question',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.blue,
+              fontFamily: 'Pacifico',
+              fontSize: 20.0,
+            ),
+          ),
+        )),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+            child: TextButton(
+              onPressed: () {},
+              style: const ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.green),
+              ),
               child: Text(
-                'Here will be the question',
+                'True',
                 style: TextStyle(
-                  color: Colors.blue,
-                  fontFamily: 'Pacifico',
                   fontSize: 20.0,
+                  color: Colors.white,
                 ),
               ),
-            )),
-
-        //Expanded(),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              onPressed: () {},
+              style: const ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.red),
+              ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
