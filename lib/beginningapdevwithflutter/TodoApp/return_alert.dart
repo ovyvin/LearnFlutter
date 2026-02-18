10b49ee0d71f123f3ea67bd936e860d8937b5823
Ovy_Vintila <ovyvin@gmail.com>
2026-02-18 10:28:45 +0000

    20260218_flutter_1

--- lib/beginningapdevwithflutter/TodoApp/return_alert.dart
+++ lib/beginningapdevwithflutter/TodoApp/return_alert.dart
@@ -4,9 +4,29 @@ class DialogBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Colors.yellow[300],
       content: Container(
         height: 120,
+        child: Column(
+          children: [
+            //get user input
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Add a new task!',
+              ),
+            ),
+
+            //buttons for input and close
+            Row(
+              children: [
+                //save button
+
+                //cancel button
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
