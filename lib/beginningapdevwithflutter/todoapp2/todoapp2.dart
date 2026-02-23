b04dbf3aa3e94c08132f5610be1ad9afa2ca337e
Ovy_Vintila <ovyvin@gmail.com>
2026-02-23 07:09:16 +0000

    20260223_flutter_1

---
+++ lib/beginningapdevwithflutter/todoapp2/todoapp2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:learnflutter/main.dart';
+
+class TodoMyApp extends StatefulWidget {
+  @override
+  _TodoMyAppState createState() => _TodoMyAppState();
+}
+
+class _TodoMyAppState extends State<TodoMyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:learnflutter/beginningapdevwithflutter/TodoApp/todoapp.dart';
-import 'package:learnflutter/beginningapdevwithflutter/TodoApp/todo_tile.dart';
+import 'package:learnflutter/beginningapdevwithflutter/todoapp2/todoapp2.dart';
+//import 'package:learnflutter/beginningapdevwithflutter/TodoApp/todo_tile.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
