067df546d1d6ba3ccefc5596192530c5edca9335
Ovy_Vintila <ovyvin@gmail.com>
2026-02-24 07:52:49 +0000

    20260224_flutter_1

--- lib/beginningapdevwithflutter/todoapp2/todoapp2.dart
+++ lib/beginningapdevwithflutter/todoapp2/todoapp2.dart
@@ -11,6 +11,18 @@ class _TodoMyAppState extends State<TodoMyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
+      appBar: AppBar(
+        centerTitle: true, //set the title to the center of the appBar
+        title: Text(
+          'To Do',
+          //textAlign: TextAlign.center, // doesn't work in appBar
+          style: TextStyle(
+            fontFamily: 'Pokemon',
+            color: Color(0xffb42920),
+          ),
+        ),
+        elevation: 0,
+      ),
     );
   }
 }
