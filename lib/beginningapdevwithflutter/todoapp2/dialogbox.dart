7dddd6a31dd829034c449e1fe2abc74e88b907c1
Ovy_Vintila <ovyvin@gmail.com>
2026-02-26 16:27:08 +0000

    20260226_flutter_4

--- lib/beginningapdevwithflutter/todoapp2/dialogbox.dart
+++ lib/beginningapdevwithflutter/todoapp2/dialogbox.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class DialogBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AlertDialog();
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content: Container(
+        height: 120,
+      ),
+    );
   }
 }
