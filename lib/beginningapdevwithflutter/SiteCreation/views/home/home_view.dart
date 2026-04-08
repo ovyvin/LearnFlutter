1802ad096724f0de9ec23cf324a95b3c544d3f8c
Ovy_Vintila <ovyvin@gmail.com>
2026-04-08 08:29:42 +0000

    20260408_flutter_4

--- lib/beginningapdevwithflutter/SiteCreation/views/home/home_view.dart
+++ lib/beginningapdevwithflutter/SiteCreation/views/home/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:learnflutter/beginningapdevwithflutter/SiteCreation/widgets/navigation_bar/navigation_bar.dart';
 import 'package:learnflutter/beginningapdevwithflutter/SiteCreation/widgets/centered_view/centered_view.dart';
 import 'package:learnflutter/beginningapdevwithflutter/SiteCreation/widgets/course_details/course_details.dart';
+import 'package:learnflutter/beginningapdevwithflutter/SiteCreation/widgets/call_to_action/call_to_action.dart';
 
 @override
 class HomeView extends StatelessWidget {
@@ -16,6 +17,13 @@ class HomeView extends StatelessWidget {
               child: Row(
                 children: [
                   CourseDetails(),
+                  Expanded(
+                    child: Center(
+                      child: CallToAction(
+                        'Join Course',
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
