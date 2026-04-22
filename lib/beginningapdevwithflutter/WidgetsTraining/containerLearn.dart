1640f2e9699b891e28628e4043773293a9e256ba
Ovy_Vintila <ovyvin@gmail.com>
2026-04-22 12:03:59 +0000

    20260422_flutter_2

--- lib/beginningapdevwithflutter/WidgetsTraining/containerLearn.dart
+++ lib/beginningapdevwithflutter/WidgetsTraining/containerLearn.dart
@@ -4,31 +4,41 @@ class ContainerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 200,
-        width: 200,
-        padding: EdgeInsets.all(20),
-        //color: Colors.red,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          //shape: BoxShape.circle,
-          color: Colors.red,
-          border: Border.all(
-            color: Colors.green,
-            width: 3,
-          ),
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.shade900,
-              blurRadius: 15,
-              spreadRadius: 1,
-              offset: Offset(-1.0, -1.0),
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Container(
+            height: 300,
+            width: 300,
+            //padding: EdgeInsets.all(20),
+            //color: Colors.red,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              //shape: BoxShape.circle,
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(15),
+              //border: Border.all(
+              //color: Colors.grey[300],
+              //   width: 3,
+              //),
+              // borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade500,
+                  blurRadius: 15,
+                  spreadRadius: 1,
+                  offset: Offset(4.0, 4.0),
+                ),
+                BoxShadow(
+                  color: Colors.white,
+                  blurRadius: 15,
+                  spreadRadius: 1,
+                  offset: Offset(-4.0, -4.0),
+                ),
+              ],
             ),
-          ],
-        ),
 
-        child: Text('Hello World!'),
+            //child: Text('Hello World!'),
+            child: Image.asset('assets/images/ball1.png')),
       ),
     );
   }
