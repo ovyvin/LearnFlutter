28b285dbcfad4b03bb462c038a48559e54dd4c50
Ovy_Vintila <ovyvin@gmail.com>
2026-04-23 06:35:29 +0000

    20260423_flutter_1

--- lib/beginningapdevwithflutter/WidgetsTraining/expandedWidget.dart
+++ lib/beginningapdevwithflutter/WidgetsTraining/expandedWidget.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 class ExpandedWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              //height: 200,
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
