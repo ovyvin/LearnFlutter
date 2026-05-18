900e342ca3fd3787f828cec5d006a03fae019a42
Ovy_Vintila <ovyvin@gmail.com>
2026-05-18 08:40:04 +0000

    20260518_Flutter_1

--- lib/beginningapdevwithflutter/Destiny2/storypage.dart
+++ lib/beginningapdevwithflutter/Destiny2/storypage.dart
@@ -1,1 +1,58 @@
 import 'package:flutter/material.dart';
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 40, horizontal: 25),
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('/assets/images/background.png'),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SafeArea(
+              child: SafeArea(
+                child: Expanded(
+                  flex: 12,
+                  child: Center(
+                    child: Text('AA'),
+                  ),
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Expanded(
+                flex: 2,
+                child: TextButton(
+                  child: Text('Choice1'),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SafeArea(
+              child: Expanded(
+                flex: 2,
+                child: TextButton(
+                  child: Text('Choise2'),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Destini',
+      title: 'Destiny',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.yellow,
