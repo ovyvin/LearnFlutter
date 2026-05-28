4571cf1c009db34f6acbb3bf93f59b6ab278f8a5
Ovy_Vintila <ovyvin@gmail.com>
2026-05-28 09:48:59 +0000

    20260528_flutter_1

--- lib/beginningapdevwithflutter/Calculator/inputpage.dart
+++ lib/beginningapdevwithflutter/Calculator/inputpage.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:learnflutter/beginningapdevwithflutter/Calculator/reusableCard.dart';
 
 class InputPage extends StatefulWidget {
@@ -21,51 +24,102 @@ class _InputPageState extends State<InputPage> {
       body: Column(
         children: [
           Expanded(
+            flex: 2,
             child: Row(
               children: [
                 Expanded(
                   child: ReusableCard(
                     colour: Color(0xFF1D1E33),
+                    cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.mars,
+                            size: 80.0,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text(
+                            'MALE',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Color(0xFF8D8E98),
+                            ),
+                          ),
+                        ]),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     colour: Color(0xFF1D1E33),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.worm,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'WOMEN',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0XFF8D8E98),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
           ),
           Expanded(
+            flex: 2,
             child: ReusableCard(
               colour: Color(0xFF1D1E33),
+              cardChild: Row(),
             ),
           ),
           Expanded(
+            flex: 2,
             child: Row(
               children: [
                 Expanded(
                   child: ReusableCard(
                     colour: Color(0xFF1D1E33),
+                    cardChild: Column(),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     colour: Color(0xFF1D1E33),
+                    cardChild: Column(),
                   ),
                 ),
               ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Color(0xFFEB1555),
+              margin: EdgeInsets.only(top: 10.0),
             ),
           ),
         ],
       ),
-      floatingActionButton: Theme(
-        data: ThemeData(primaryColor: Colors.green),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
-        ),
-      ),
+
+      // floatingActionButton: Theme(
+      //   data: ThemeData(primaryColor: Colors.green),
+      //   child: FloatingActionButton(
+      //     child: Icon(Icons.add),
+      //     onPressed: () {},
+      //   ),
+      // ),
     );
   }
 }
--- lib/beginningapdevwithflutter/Calculator/reusableCard.dart
+++ lib/beginningapdevwithflutter/Calculator/reusableCard.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  Color? colour;
-  ReusableCard({@required this.colour});
+  final Color? colour;
+  final Widget cardChild;
+
+  ReusableCard({@required this.colour, required this.cardChild});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
