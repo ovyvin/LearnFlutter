ed81fd8edd1fb493054152d5bed2b14018abce16
Ovy_Vintila <ovyvin@gmail.com>
2026-06-22 07:05:34 +0000

    20260622_Flutter_1

--- lib/beginningapdevwithflutter/Calculator/inputpage.dart
+++ lib/beginningapdevwithflutter/Calculator/inputpage.dart
@@ -280,7 +280,12 @@ class _InputPageState extends State<InputPage> {
                   MaterialPageRoute(builder: (context) => ResultsPage()));
             },
             child: Container(
-              child: Text('CALCULATE'),
+              child: Center(
+                child: Text(
+                  'CALCULATE',
+                  style: kLargeButtonTextStyle,
+                ),
+              ),
               color: Color(0xFFEB1555),
               margin: EdgeInsets.only(top: 10.0),
               height: 45.0,
