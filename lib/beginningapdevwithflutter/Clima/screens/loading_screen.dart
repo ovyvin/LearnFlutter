ead6c64ecd7bd3968b935a33e4f3afb248fa3116
Ovy_Vintila <ovyvin@gmail.com>
2026-07-14 07:16:41 +0000

    20260714_flutter_1

--- lib/beginningapdevwithflutter/Clima/screens/loading_screen.dart
+++ lib/beginningapdevwithflutter/Clima/screens/loading_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:learnflutter/beginningapdevwithflutter/Clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -21,6 +24,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://my.meteoblue.com/packages/basic-1h_basic-day?lat=47.558&lon=7.573&apikey=CWBXAKz0pT1MLsUc'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(response.statusCode);
+      var decodedData = jsonDecode(data);
+      var weatherDescription = decodedData['data_1h']['temperature'][0];
+      print(weatherDescription);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +45,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: TextButton(
           onPressed: () {
             getLocation();
+            getData();
           },
           child: Text('Get Location'),
         ),
