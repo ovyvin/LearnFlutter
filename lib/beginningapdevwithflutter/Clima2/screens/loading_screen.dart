a29e6dcd5a3ee3ad9440611a5849c31e7a94dacc
Ovy_Vintila <ovyvin@gmail.com>
2026-08-11 13:06:08 +0000

    20260811_flutter_3

--- lib/beginningapdevwithflutter/Clima2/screens/loading_screen.dart
+++ lib/beginningapdevwithflutter/Clima2/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 import 'package:learnflutter/beginningapdevwithflutter/Clima2/services/location.dart';
 
 class LoadScreen extends StatefulWidget {
@@ -8,20 +9,23 @@ class LoadScreen extends StatefulWidget {
 }
 
 class _LoadScreenState extends State<LoadScreen> {
+  @override
+  initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
   }
 
   // void somethinThatExpects(int n) {
   //   if (n > 10) {
   //     throw 'n is greater than 10, not ok';
   //   }
-  // }
-
-  // initState() {
-  //   super.initState();
-  //   getLocation();
   // }
 
   @override
