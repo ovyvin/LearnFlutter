d5d04ea1714c37bbf9b39aa28b69e4fc9a439d8b
Ovy_Vintila <ovyvin@gmail.com>
2026-08-20 05:48:49 +0000

    20260820_Flutter_3

--- lib/beginningapdevwithflutter/Clima2/screens/city_screen.dart
+++ lib/beginningapdevwithflutter/Clima2/screens/city_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learnflutter/beginningapdevwithflutter/Clima/screens/location_screen.dart';
 import 'package:learnflutter/beginningapdevwithflutter/Clima2/utilities/constants.dart';
 
 class CityScreen extends StatefulWidget {
@@ -13,7 +14,7 @@ class _CityScreenState extends State<CityScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/city_background.jpg'),
+            image: AssetImage('assets/images/city_background.jpg'),
             fit: BoxFit.cover,
           ),
         ),
@@ -24,7 +25,16 @@ class _CityScreenState extends State<CityScreen> {
               Align(
                 alignment: Alignment.topLeft,
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pop(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LocationScreen();
+                        },
+                      ),
+                    );
+                  },
                   child: Icon(
                     Icons.arrow_back_ios,
                     size: 50.0,
