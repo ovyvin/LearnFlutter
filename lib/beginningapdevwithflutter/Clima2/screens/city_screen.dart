aa43243705101e4ce0b8a39db30d0dbaf26f2b31
Ovy_Vintila <ovyvin@gmail.com>
2026-08-21 05:35:17 +0000

    20260821_flutter_1

--- lib/beginningapdevwithflutter/Clima2/screens/city_screen.dart
+++ lib/beginningapdevwithflutter/Clima2/screens/city_screen.dart
@@ -43,7 +43,17 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    icon: Icon(
+                      Icons.location_city,
+                      color: Colors.white,
+                    ),
+                    hintText: 'Enter City Name',
+                  ),
+                ),
               ),
               TextButton(
                 onPressed: () {},
